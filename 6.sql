f4908904816d36b53a5fb84909996f74ecdfe5b1
Senay Yakut <senaykt@gmail.com>
2020-08-01 20:39:54 -0700

    Movies released 2010 and their ratings according to IMDb open source data

---
+++ 6.sql
@@ -0,0 +1,7 @@
+-- In 6.sql, write a SQL query to determine the average rating of all movies released in 2012
+
+SELECT AVG(rating)
+FROM ratings
+JOIN movies
+AS ratings.movie_id = movies.id
+WHERE year = 2012;
