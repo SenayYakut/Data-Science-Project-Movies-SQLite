6b5e295fdb39759ea544c7dff84ce3970378d8c2
Senay Yakut <senaykt@gmail.com>
2020-08-01 18:12:19 -0700

    Movies got 10 rating according to IMDb database

---
+++ 4.sql
@@ -0,0 +1,6 @@
+-- In 4.sql, write a SQL query to determine the number of movies with an IMDb rating of 10.0.
+SELECT COUNT(*)
+FROM movies
+JOIN ratings
+ON movies.id = ratings.movie_id
+WHERE rating = 10.0;
